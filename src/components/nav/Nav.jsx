371be63981071ed58db2b2b90a9b4388ccec144a
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-04-20 20:14:29 +0900

    Managing Nav States using useState Hook

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import './nav.css';
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineUser } from 'react-icons/ai';
@@ -10,27 +10,52 @@ import { MdOutlineComputer } from 'react-icons/md';
 import { BsFillPersonFill } from 'react-icons/bs';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href="#" className="active">
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav('#about')}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav('#services')}
+        className={activeNav === '#services' ? 'active' : ''}
+      >
         <RiServiceLine />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav('#experience')}
+        className={activeNav === '#experience' ? 'active' : ''}
+      >
         <BiBook />
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav('#portfolio')}
+        className={activeNav === '#portfolio' ? 'active' : ''}
+      >
         <MdOutlineComputer />
       </a>
-      <a href="#testimonials">
+      <a
+        href="#testimonials"
+        onClick={() => setActiveNav('#testimonials')}
+        className={activeNav === '#testimonials' ? 'active' : ''}
+      >
         <BsFillPersonFill />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav('#contact')}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
         <BiMessageSquareDetail />
       </a>
     </nav>
