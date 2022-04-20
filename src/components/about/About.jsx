6347195e35ebf6fc0ea5d130edf2be6f89d45221
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-04-20 20:47:50 +0900

    About Component Update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,57 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me-about.jpg';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { IoIosConstruct } from 'react-icons/io';
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>私について</h5>
+      <h2>自己紹介</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Web系言語の独学年数</h5>
+              <small>2年</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>IT業界の勤続年数</h5>
+              <small>5年以上(2022年現在)</small>
+            </article>
+            <article className="about__card">
+              <IoIosConstruct className="about__icon" />
+              <h5>業務で経験してきたこと</h5>
+              <small>Ciscoルータ・スイッチの設定投入、機器設置</small>
+              <small>Linuxを用いたインフラ基盤の構築</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo at blanditiis ut
+            provident distinctio maxime quidem pariatur necessitatibus, quas adipisci doloremque,
+            quaerat placeat? Saepe corrupti sunt, fugiat rem consequuntur accusantium.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            連絡先はこちらより
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
