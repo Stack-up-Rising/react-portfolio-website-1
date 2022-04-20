586466a3a072518dbdf087855b3d4f1a89797f4d
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-04-20 19:35:08 +0900

    Floating Nav Component version2

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './nav.css';
 import { AiOutlineHome } from 'react-icons/ai';
@@ -5,21 +6,29 @@ import { AiOutlineUser } from 'react-icons/ai';
 import { BiBook } from 'react-icons/bi';
 import { RiServiceLine } from 'react-icons/ri';
 import { BiMessageSquareDetail } from 'react-icons/bi';
+import { MdOutlineComputer } from 'react-icons/md';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 const Nav = () => {
   return (
     <nav>
-      <a href="#" className='active'>
+      <a href="#" className="active">
         <AiOutlineHome />
       </a>
       <a href="#about">
         <AiOutlineUser />
       </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
       <a href="#experience">
         <BiBook />
       </a>
-      <a href="#services">
-        <RiServiceLine />
+      <a href="#portfolio">
+        <MdOutlineComputer />
+      </a>
+      <a href="#testimonials">
+        <BsFillPersonFill />
       </a>
       <a href="#contact">
         <BiMessageSquareDetail />
